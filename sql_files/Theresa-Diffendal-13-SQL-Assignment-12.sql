ac5251dd9940111dccf3c9a99e14ea05c3551fe2
tdiffendal <42811260+tdiffendal@users.noreply.github.com>
2019-03-13 22:21:07 -0400

    update hw

--- sql_files/Theresa-Diffendal-13-SQL-Assignment-12.sql
+++ sql_files/Theresa-Diffendal-13-SQL-Assignment-12.sql
@@ -34,11 +34,54 @@ From bard.accidents a Join bard.deaths d on a.bardid = d.bardid
 Where a.NameOfBodyOfWater = "Potomac River"
 Group by a.bardid, a.NameOfBodyOfWater, d.DeceasedPFDWorn;
 
-select * from bard.accidents
+#Just to check and see how many records there were, 3
+select DeceasedPFDWorn 
+From bard.accidents a Join bard.deaths d on a.bardid = d.bardid
 Where NameOfBodyOfWater = "Potomac River";
 
 # 4: You're writing a story about the role alcohol plays in boating accidents and are looking for a detail for a story. A Coast Guard source tells you he remembered an accident that caused thousands of dollars in damage after the occupants downed more than a dozen small tubs of beer (an average one of these containers holds about 6 bottles of beer) at a bar before hitting the water some time in the last decade.  You try to find it in the data. How many of these tubs did they pay for?
+#17 buckets of beer, probably a better way to do it since I read many narratives
+select a.RedactedNarrative, a.TotalDamage, a.AccidentCause1, a.AccidentCause2, a.AccidentCause3 
+From bard.accidents a Join bard.deaths d on a.bardid = d.bardid
+Where a.AlcoholInvolved = "Yes"
+or a.AccidentCause1 like "%Alcohol%"
+or a.AccidentCause2 like "%Alcohol%"
+or a.AccidentCause3 like "%Alcohol%"
+Group by a.RedactedNarrative, a.TotalDamage, a.AccidentCause1, a.AccidentCause2, a.AccidentCause3 
+Order by a.TotalDamage desc;
+
+select * from bard.accidents
+where RedactedNarrative IS NOT NULL
+and TotalDamage > 0
+and AlcoholInvolved = "yes"
+order by TotalDamage desc;
 
 # 5: How many accidents were there where at least one person who wasn't the operator of a boat (an occupant) died?
+#67? Slightly unsure about my or statement
+select a.bardid, a.Numberdeaths, d.DeceasedRole, d.DeceasedRoleOther
+From bard.accidents a Join bard.deaths d on a.bardid = d.bardid
+Where (d.DeceasedRole <> "Operator" or d.DeceasedRoleOther <> "Operator")
+group by a.bardid, a.Numberdeaths, d.DeceasedRole, d.DeceasedRoleOther;
+
+select * from bard.deaths;
+select * from bard.accidents;
 
 # 6 - #8 Think of three interesting questions to ask and answer of the data.  At least two of them should join accidents to the death table.
+
+#What state saw the most deaths? - Maryland with 133
+select a.state, count(a.numberdeaths)
+From bard.accidents a Join bard.deaths d on a.bardid = d.bardid
+group by a.state
+order by count(a.numberdeaths) desc;
+
+#How many deaths were on a clear day when a lifevest wasn't worn? - 19 times
+select count(*)
+From bard.accidents a Join bard.deaths d on a.bardid = d.bardid
+where a.Clear = "-1"
+and d.DeceasedPFDWorn = "-1";
+
+#what was the average age of those who died when alcohol was involved? 
+#When involved 39.22, when not involved 48.2
+select a.AlcoholInvolved, avg(d.DeceasedAge)
+From bard.accidents a Join bard.deaths d on a.bardid = d.bardid
+Group by a.AlcoholInvolved;
